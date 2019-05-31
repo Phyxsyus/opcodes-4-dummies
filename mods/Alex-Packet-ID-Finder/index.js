e652af3b9de22ea6d63a5a6b6a158036dc909833
海比丶天蓝 <149352394@qq.com>
2019-06-01 00:15:23 +0800

    zc

--- mods/Alex-Packet-ID-Finder/index.js
+++ mods/Alex-Packet-ID-Finder/index.js
@@ -1,26 +1,25 @@
 const { protocol } = require('tera-data-parser')
 
-const Command = require('command')
 const path = require('path')
 const fs = require('fs')
 
-protocol.load(require.resolve('tera-data'));
+protocol.load(require.resolve('tera-data'))
 
-module.exports = function AlexPacketIdFinder(dispatch) {
-    const command = Command(dispatch)
+module.exports = function AlexPacketIdFinder(mod) {
+	const command = mod.command
 	
 	let enabled = false
 	let fullPacketDefList = [...new Set(findPacketDefList())]
-	let filteredPacketDefList = null
+	let filteredPacketDefList = fullPacketDefList
 	let filterExpression = '.*'
 	let filterKnownPackets = true
 	let packetId = null
-	let showCandidateJson = false
+	let showCandidateJson = true
 	
-	function printMainStatus()
-	{
+	function printMainStatus() {
 		if (enabled) {
 			command.message(`Packet id finder is now enabled (${packetId !== null ? 'only id ' + packetId : 'any id'}, regex /${filterExpression}/i).`)
+			protocol.maps.get(mod.dispatch.protocolVersion).name.set(filterExpression, 0)
 		} else {
 			command.message(`Packet id finder is now disabled.`)
 		}
@@ -30,19 +29,18 @@ module.exports = function AlexPacketIdFinder(dispatch) {
 		if (arg1 !== undefined) arg1 = ''+arg1
 		if (arg2 !== undefined) arg2 = ''+arg2
 		
-		
 		if (arg1 === undefined || ['d', 'disabled', 'false', 'no', '0'].includes(arg1.toLowerCase())) {
 			enabled = false
 			packetId = null
 			filterExpression = '.*'
-			filteredPacketDefList = null
+			rebuildFilteredPacketDefList()
 			
 			printMainStatus()
 		} else if (/^\d+$/.test(arg1)) {
 			enabled = true
 			packetId = parseInt(arg1)
 			filterExpression = '.*'
-			filteredPacketDefList = null
+			rebuildFilteredPacketDefList()
 			
 			if (arg2 !== undefined) {
 				filterExpression = arg2
@@ -55,12 +53,13 @@ module.exports = function AlexPacketIdFinder(dispatch) {
 				command.message(`Showing candidates as JSON is now ${showCandidateJson ? 'enabled' : 'disabled'}.`)
 			} else if (['k', 'known', 'u', 'unk', 'unknown'].includes(arg1)) {
 				filterKnownPackets = !filterKnownPackets
+				rebuildFilteredPacketDefList()
 				command.message(`Known packet filtering is now ${filterKnownPackets ? 'enabled' : 'disabled'}.`)
 			} else {
 				enabled = true
 				packetId = null
 				filterExpression = arg1
-				filteredPacketDefList = null
+				rebuildFilteredPacketDefList()
 				
 				printMainStatus()
 			}
@@ -68,23 +67,23 @@ module.exports = function AlexPacketIdFinder(dispatch) {
 		
 	})
 	
-	function findPacketDefList()
-	{
+	function findPacketDefList() {
 		let result = []
 		let basePath = require.resolve('tera-data')
 		if (path.basename(basePath) === 'package.json') {
-			basePath = path.dirname(basePath);
+			basePath = path.dirname(basePath)
 		}
+		
 		let defPath = path.join(basePath, 'protocol')
 		let defFiles = fs.readdirSync(defPath)
 		for (let file of defFiles) {
 			let fullpath = path.join(defPath, file)
-
+			
 			let parsedName = path.basename(file).match(/^(\w+)\.(\d+)\.def$/)
 			if (!parsedName) {
-				continue;
+				continue
 			}
-
+			
 			let name = parsedName[1]
 			result.push(name)
 		}
@@ -92,8 +91,7 @@ module.exports = function AlexPacketIdFinder(dispatch) {
 		return result
 	}
 	
-	function isDefPerhapsWrong(name, packet, incoming, data, code) 
-	{
+	function isDefPerhapsWrong(name, packet, incoming, data, code) {
 		if (incoming && name.slice(0, 2) === 'C_') {
 			return true
 		}
@@ -101,41 +99,37 @@ module.exports = function AlexPacketIdFinder(dispatch) {
 			return true
 		}
 		
-		let protocolVersion = dispatch.base.protocolVersion
+		let protocolVersion = mod.dispatch.protocolVersion
 		let data2 = protocol.write(protocolVersion, name, '*', packet, undefined, undefined, code)
-		if ((data.length != data2.length) || !data.equals(data2)) {
+		if ((data.length != data2.length) || !data.equals(data2)) { // type Buffer
 			return true
 		} else {
 			return false
 		}
 	}
 	
-	function rebuildFilteredPacketDefList()
-	{
+	function rebuildFilteredPacketDefList() {
 		filteredPacketDefList = []
 		let re = new RegExp(filterExpression, 'i')
 		for (let name of fullPacketDefList) {
-			let code = protocol.maps.get(dispatch.base.protocolVersion).name.get(name)
-			let known = (code !== undefined && code !== null)
-			if (known && filterKnownPackets) continue
+			let code = protocol.maps.get(mod.dispatch.protocolVersion).name.get(name)
+			let known = (code !== undefined && code !== null && code !== 0)
+			if (known && filterKnownPackets) {
+				//console.log("known " + name)
+				continue
+			}
 			
-			if (re.test(name)) {	
+			if (re.test(name)) {
+				//console.log(name)
 				filteredPacketDefList.push(name)
 			}
 		}
 	}
 	
-	function findPacketIds(code, data, incoming, fake)
-	{
+	function findPacketIds(code, data, incoming, fake) {
 		let result = []
 		
-		let packetDefList = filteredPacketDefList
-		if (packetDefList === null) {
-			rebuildFilteredPacketDefList()
-			packetDefList = filteredPacketDefList
-		}
-		
-		for (let name of packetDefList) {
+		for (let name of filteredPacketDefList) {
 			if (incoming && name.slice(0, 2) === 'C_') {
 				continue
 			}
@@ -143,25 +137,32 @@ module.exports = function AlexPacketIdFinder(dispatch) {
 				continue
 			}
 			try {
-				let protocolVersion = dispatch.base.protocolVersion
+				let protocolVersion = mod.dispatch.protocolVersion
 				let packet = protocol.parse(protocolVersion, name, '*', data)
 				let defPerhapsWrong = isDefPerhapsWrong(name, packet, incoming, data, code)
 				
 				if (!defPerhapsWrong) {
 					result.push(name)
 				}
-			} catch(e) {
+			} catch(e) { //console.log(e)
 			}
 		}
-		 
+		
 		return result
-    }
-
-    dispatch.hook('*', 'raw', { order: 999, type: 'all' }, (code, data, incoming, fake) => {
+	}
+	
+	function loopBigIntToString(obj) {
+		Object.keys(obj).forEach(key => {
+			if (obj[key] && typeof obj[key] === 'object') loopBigIntToString(obj[key])
+			else if (typeof obj[key] === "bigint") obj[key] = obj[key].toString()
+		})
+	}
+	
+	mod.hook('*', 'raw', { order: 999, type: 'all' }, (code, data, incoming, fake) => {
 		if (!enabled) return
 		if (packetId !== null && code != packetId) return
 		
-		let protocolVersion = dispatch.base.protocolVersion
+		let protocolVersion = mod.dispatch.protocolVersion
 		let name = null
 		let packet = null
 		
@@ -171,19 +172,25 @@ module.exports = function AlexPacketIdFinder(dispatch) {
 			name = undefined
 		}
 		
-		if (name === undefined || name === null) {
-			candidates = findPacketIds(code, data, incoming, fake)
+		let known = (name !== undefined && name !== null)
+		
+		if (!known || !filterKnownPackets) {
+			let candidates = findPacketIds(code, data, incoming, fake)
 			if (candidates.length > 0) {
 				console.log(`Candidates for id ${code}: [${candidates.join(', ')}].`)
+				command.message(`Candidates for id ${code}: [${candidates.join(', ')}].`)
 				if (showCandidateJson) {
 					for (let candidate of candidates) {
 						let packet = protocol.parse(protocolVersion, candidate, '*', data)
 						console.log(`${code} as ${candidate}:`)
+						loopBigIntToString(packet)
 						let json = JSON.stringify(packet, null, 4)
 						console.log(json)
+						command.message(json)
 					}
 				}
 			}
 		}
-    })
-};
+	})
+	
+}
